640158e8120f3017127febabc68f87757cd4a3c7
Sejotaz <alejospina08@gmail.com>
2024-04-03 11:19:39 -0500

    Eventos click (Eventos en general)

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,19 +1,25 @@
-import  PropTypes  from "prop-types";
+import { useState } from "react"
+import PropTypes from "prop-types"
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({ value }) => {
+
+  const [] = useState()
+
+  const handleAdd = () => { }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   )
 }
 
 CounterApp.propTypes = {
-  value: PropTypes.number
+  value: PropTypes.number,
 }
 
 CounterApp.defaultProps = {
-  value: 0
+  value: 0,
 }
-
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HelloWorldApp } from './HelloWorldApp'
-import { FirstApp } from './FirstApp'
 import './styles.css'
 import { CounterApp } from './CounterApp'
 
